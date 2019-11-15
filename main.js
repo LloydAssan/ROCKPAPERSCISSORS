cae4cb5cd5f51c72d47bdf49e522415ba4019e45
Lloyd Assan <55921640+LloydAssan@users.noreply.github.com>
2019-11-15 18:43:12 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+let robotScore = 0,
+    playerScore = 0,
+    rock = document.getElementById("rock"),
+    paper = document.getElementById("paper"),
+    scissors = document.getElementById("scissors"),
+    lizard = document.getElementById("lizard"),
+    spork = document.getElementById("spork")
+
+
+
+document.getElementById("rock").addEventListener("click", ()=>{
+  console.log("Rock it")
+  function playerPlaysRock(){
+    if(button == rock){
+      document.
+    }
+  }
+})
+
+document.getElementById("paper").addEventListener("click", ()=>{
+  console.log("Paper that")
+  function playerPlaysPaper(){
+
+  }
+})
+
+document.getElementById("scissors").addEventListener("click", ()=>{
+  console.log("Scissors it")
+  function playerPlaysScissors(){
+
+  }
+})
+
+document.getElementById("lizard").addEventListener("click", ()=>{
+  console.log("Lizards?")
+  function playerPlaysLizard(){
+
+  }
+})
+
+document.getElementById("spork").addEventListener("click", ()=>{
+  console.log("Spork it")
+  function playerPlaysSpork(){
+
+  }
+})
